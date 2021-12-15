5c1a7c46fa81144e8c21799c39ad746f97886986
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2021-12-14 16:58:15 -0800

    final

--- src/components/UserForms.jsx
+++ src/components/UserForms.jsx
@@ -2,7 +2,8 @@ import React, { useState } from  'react';
 
 
 const UserForm = () => {
-    const [username, setFName] = useState("");
+    const [firstName, setFName] = useState("");
+    const [lastName, setLName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPass, setConfirmPass] = useState("");
@@ -12,31 +13,38 @@ const UserForm = () => {
         <div>
             <form className='Form' onSubmit={()=>{}}>
                 <div className='field'>
-                    <label>Username: </label>
-                    <input type="text" name='username' onChange={(e) => setFName(e.target.value)} />
+                    <label>First Name: </label>
+                    <input type="text" onChange={(e) => setFName(e.target.value)} />
                 </div>
-                {username.length > 4 && username.length < 0 ?
-                    <p>Username must be at least 4 characters</p> : null
+                {firstName.length < 2 && firstName.length > 0 ?
+                    <p>First name must be at least 2 characters</p> : null
+                }
+                <div className='field'>
+                    <label>Last Name: </label>
+                    <input type="text" onChange={(e) => setLName(e.target.value)} />
+                </div>
+                {lastName.length < 2 && lastName.length > 0 ?
+                    <p>Last name must be at least 2 characters</p> : null
                 }
                 <div className='field'>
                     <label>Email: </label>
                     <input type="text" onChange={(e) => setEmail(e.target.value)} />
                 </div>
-                {email.length > 6 && email.length < 0 ?
-                    <p>Email must be at least 6 characters</p> : null
+                {email.length < 5 && email.length > 0 ?
+                    <p>Email must be at least 5 characters</p> : null
                 }
                 <div className='field'>
                     <label>Password: </label>
                     <input type="text" onChange={(e) => setPassword(e.target.value)} />
                 </div>
-                {password.length > 4 && password.length < 0 ?
-                    <p>password must be at least 4 characters</p> : null
+                {password.length < 8 && password.length > 0 ?
+                    <p>Password must be at least 8 characters</p> : null
                 }
                 <div className='field'>
                     <label>Confirm Password: </label>
                     <input type="text" onChange={(e) => setConfirmPass(e.target.value)} />
                 </div>
-                {confirmPass !== password && confirmPass.length < 0 ?
+                {confirmPass !== password && confirmPass.length > 0 ?
                     <p>Passwords do not match</p> : null
                 }
                 <input type="submit" value="Create" />
